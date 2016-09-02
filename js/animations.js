f41d73bedede160865f00230e0d29001dc0f1024
JJ Crossman <jj.crossman@gmail.com>
2016-09-02 18:15:25 -0500

    working on twitter clone

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,11 @@
 $(document).ready( function() {
     //hidden on load
-    $( '#tweet-controls, .stats, .reply' ).css( "display", "none" );
-    $( '.tweet-actions' ).animate( {opacity: 0}, 0 );
+    function displayNone() {
+      $( '#tweet-controls, .stats, .reply' ).css( "display", "none" );
+      $( '.tweet-actions' ).animate( {opacity: 0}, 0 );
+    };
+
+    displayNone();
 
     //variables
     var newTweetHeight = $( '#tweet-content > .tweet-compose' ).height();
@@ -60,9 +64,13 @@ $(document).ready( function() {
         .find( '.stats, .reply' ).css( "display", "block");
     });
 
+    var newTweet = "<div class=\"tweet\">      <div class=\"content\">        <img class=\"avatar\" src=\"img/alagoon.jpg\" />        <strong class=\"fullname\">My BFF</strong>        <span class=\"username\">@mybff</span>        <p class=\"tweet-text\">Today is an amazing day.</p>   <div class=\"tweet-actions\">          <ul>            <li><span class=\"icon action-reply\"></span> Reply</li>            <li><span class=\"icon action-retweet\"></span> Retweet</li>            <li><span class=\"icon action-favorite\"></span> Favorite</li>            <li><span class=\"icon action-more\"></span> More</li>          </ul>        </div>        <div class=\"stats\">          <div class=\"retweets\">            <p class=\"num-retweets\">30</p>            <p>RETWEETS</p>          </div>          <div class=\"favorites\">            <p class=\"num-favorites\">6</p>            <p>FAVORITES</p>          </div>          <div class=\"users-interact\">            <div>              <img src=\"img/alagoon.jpg\" />              <img src=\"img/vklimenko.jpg\" />            </div>          </div> <div class=\"time\">1:04 PM - 19 Sep 13</div></div>        <div class=\"reply\">          <img class=\"avatar\" src=\"img/alagoon.jpg\" />          <textarea class=\"tweet-compose\" placeholder=\"Reply to @mybff\"/></textarea></div></div></div>"
 
+    $( '#tweet-submit' ).click( function () {
+      $( '#stream' ).append( newTweet );
+      displayNone();
 
-
+    });
 
 
 
